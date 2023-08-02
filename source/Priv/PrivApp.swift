ca5f39407c8592cd3942bf63d12f71404008e660
Ahmed Theba <ahmed.theba@securrency.com>
2023-08-02 13:24:37 +0200

    Fork
    This fork disables the main screen button (which breaks automated revert of local admin)

---
+++ source/Priv/PrivApp.swift
@@ -0,0 +1,18 @@
+//
+//  PrivApp.swift
+//  Priv
+//
+//  Created by Ahmed Theba on 2023/08/02.
+//  Copyright © 2023 SAP SE. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PrivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ source/PrivilegesSecurrency/PrivilegesSecurrencyApp.swift
@@ -0,0 +1,18 @@
+//
+//  PrivilegesSecurrencyApp.swift
+//  PrivilegesSecurrency
+//
+//  Created by Ahmed Theba on 2023/08/02.
+//  Copyright © 2023 SAP SE. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PrivilegesSecurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
